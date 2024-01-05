25491e7ec53981208fd8c774007c1cbfc12f9bcc
tyson <83587481+tysonlmao@users.noreply.github.com>
2024-01-06 00:13:07 +1100

    fixes

--- dashboard/index.php
+++ dashboard/index.php
@@ -39,15 +39,7 @@ if ($_SESSION['cuid_role'] === "default") {
             decoder: {
                 decoder: {
                     readers: [
-                        "code_128_reader",
-                        "ean_reader",
-                        "ean_8_reader",
-                        "code_39_reader",
-                        "code_39_vin_reader",
-                        "codabar_reader",
-                        "upc_reader",
-                        "upc_e_reader",
-                        "i2of5_reader"
+                        "code_128_reader"
                     ]
                 }
             }
